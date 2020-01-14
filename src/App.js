91591d6486617701c6b633a848216e8575f579a8
man <man@outlook.jp>
2020-01-14 16:07:09 +0900

    a

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <input type="button" value="click" onClick={()=>{console.log(20)}} />;
+    return <input type="button" value="click" onClick={()=>{console.log(30)}} />;
   }
 }
 
