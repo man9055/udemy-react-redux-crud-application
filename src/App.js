d79c71a0e2b32fe26752b187aec586ac02adbc3b
man <man@outlook.jp>
2020-01-14 15:56:48 +0900

    new save

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return  <div><h1>Hello, world!</h1></div>;
+    return <input type="button" value="click" onClick={()=>{console.log(10)}} />;
   }
 }
 
