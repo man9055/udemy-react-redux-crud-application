36110b82d55c2a7472a8437597f9901eb19fa8fa
man <man@outlook.jp>
2020-01-15 15:47:43 +0900

    aaaaa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <input type="button" value="click" onClick={()=>{console.log(50)}} />;
-  }
-}
+const App = () => ()
+
 
 export default App;
