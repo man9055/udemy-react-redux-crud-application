bd730f0faad831a3e625af1d4f02f38d24fbf712
man <man@outlook.jp>
2020-01-16 17:32:34 +0900

    a

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const App = () => ()
+class App extends Components {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  handlePlusButton = () => {
+    this.setState({
+      count: this.state + 1
+    })
+  }
+
+  handleMinusButton = () => {
+    this.setState({
+      count: this.state - 1
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+      <div>count: {this.state}</div>
+      <button onClick={this.handlePlusButton}>+1</button>
+      <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
+}
 
 
 export default App;
